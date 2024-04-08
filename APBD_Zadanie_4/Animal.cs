e39e0dcd76711ae13f949e297f050d02f8ebbc87
s19787 <s19787@pjwstk.edu.pl>
2024-04-08 18:49:19 +0200

    updates

--- APBD_Zadanie_4/Animal.cs
+++ APBD_Zadanie_4/Animal.cs
@@ -1,8 +1,11 @@
-﻿namespace APBD_Zadanie_4
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace APBD_Zadanie_4
 {
     public class Animal
     {
         public int Id { get; set; }
+        [Required] //validation example
         public string Name { get; set; }
         public string Category { get; set; }
         public double Weight { get; set; }
--- APBD_Zadanie_4/Controllers/AnimalsController.cs
+++ APBD_Zadanie_4/Controllers/AnimalsController.cs
@@ -13,5 +13,26 @@ namespace APBD_Zadanie_4.Controllers
             return Ok(AnimalsDataStore.Current.Animals);
             //comment test
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Animal> GetAnimal(int id)
+        {
+            var animalToReturn = AnimalsDataStore.Current.Animals.FirstOrDefault(x => x.Id == id);
+
+            if (animalToReturn == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(animalToReturn);
+        }
+
+        [HttpPost]
+        public ActionResult<Animal> CreateAnimal(AnimalCreationDTO animal) //tp samo co animal ale bez id
+        {
+            var maxAnimalId = tmp;
+            
+            //to do put -> return NoContent();
+        }
     }
 }
